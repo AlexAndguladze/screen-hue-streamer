d599230d3ed3c6e3cf58351b6065056c72959635
FASTPAY\a.andghuladze <andghuladze.alex@gmail.com>
2024-04-30 16:31:07 +0400

    add main window. use wxWidget. update gitignore

---
+++ App.cpp
@@ -0,0 +1,11 @@
+#include "App.h"
+#include <wx/wx.h>
+#include "MainFrame.h"
+
+wxIMPLEMENT_APP(App);
+
+bool App::OnInit() {
+	MainFrame* mainFrame = new MainFrame("Screen Hue Streamer");
+	mainFrame->Show();
+	return true;
+}
